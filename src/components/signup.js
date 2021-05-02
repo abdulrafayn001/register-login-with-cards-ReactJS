e6c1edd3d9b4e55d20da1372b10edc18892785bc
Abdulrafay <abdulrafayn001@gmail.com>
2021-05-01 21:44:29 -0700

    Add JSON Server and Complete SignUp backend

--- src/components/signup.js
+++ src/components/signup.js
@@ -9,7 +9,8 @@ class SignUp extends React.Component {
             username:'',
             email:'',
             password:'',
-            contact:''
+            contact:'',
+            data:null
         }
     }
 
@@ -31,7 +32,46 @@ class SignUp extends React.Component {
 
     handleSubmit=(event)=> {
         event.preventDefault();
-        alert('A name was submitted: ' + this.state.username);
+        let allEmail=this.fetchEmils()
+
+        const user={
+            username:this.state.username,
+            email:this.state.email,
+            password:this.state.password,
+            contact:this.state.contact,
+            products:{}
+        }
+        console.log(this.state.email)
+        console.log(allEmail)
+        console.log(allEmail.indexOf("abdulrafayn001@gmail.com"))
+        allEmail.indexOf(this.state.email)>-1 ? alert("Email alreday exists"):this.registerUser(user)
+        
+    }
+
+    componentDidMount() {
+        fetch(`http://localhost:8000/users`)
+          .then(res => res.json())
+          .then(json => this.setState({ data: json }));
+      }
+
+    fetchEmils=()=>{
+        let allEmail=[]
+        this.state.data.map((d)=>{
+            allEmail.push(d.email);
+            return allEmail
+        })
+        return allEmail
+    }
+
+    registerUser=(user)=>{
+        
+        fetch('http://localhost:8000/users',{
+            method:'POST',
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(user)
+            }).then(()=>{
+                alert('Registered');
+            })
     }
 
     render() { 
